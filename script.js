5e2a225445d4756fff566fd58d48faf20a267e4a
LinconCS <linconsantos@gmail.com>
2023-04-25 21:06:42 -0300

    alteracoes-dom-1-exercicios

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+//I)
 const frutas = ["laranja", "limão", "uva"]
+
+/*const laranja = document.querySelector('#fruta-1')
+laranja.innerHTML = frutas[0]
+
+const limao = document.getElementById('fruta-2')
+limao.innerHTML = frutas[1]
+
+const uva = document.getElementById('fruta-3')
+uva.innerHTML = frutas[2]
+*/
+
+const frutaParaDeterminar = document.getElementById('fruta-4')
+
+// outra forma
+const frutasDoHtml = document.querySelectorAll('#lista-de-frutas li')
+console.log(frutasDoHtml)
+
+for(let i = 0; i < frutas.length; i++) {
+    frutasDoHtml[i].innerHTML = frutas[i]
+}
+
+//II)
+
+let input = document.getElementById('texto')
+
+const imprimeTexto = () => {
+    console.log(texto.value)
+}
+
+//III)
+const addInList = () => {
+    frutaParaDeterminar.innerHTML = texto.value
+}
